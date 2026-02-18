4252cc9068cca21b82a9af9276889cde616cf195
Saqib2029 <saqib98ma@gmail.com>
2026-02-18 12:05:46 +0530

    Add files via upload

--- program13.c
+++ program13.c
@@ -1,8 +1,26 @@
-
-//wap to find roots of a quadratic equation
-#include <stdio.h>
+// program 13 to check the number is =,-,0
+#include<stdio.h>
 int main()
 {
+
     int n;
-    printf()
+    printf("enter a number toh check");
+    scanf("%d",&n);
+    
+    if(n>0)
+    {
+    printf("number is positive");
+    }
+    else if(n<0)
+    {
+    printf("number is negative");
+    }
+    else
+    {
+    printf("number is zero");
+        
+    }
+    
+    
+    return 0;
 }
